756552d5a18dcb94fa7df6c259d2f4fb7afa2319
IvanPolienko <piar1986@yandex.ru>
2020-03-09 20:56:45 +0700

    Added task for verification

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# your models here
+class Pokemon(models.Model):
+    '''Покемон'''
+    title_ru = models.CharField('Название на русском языке', max_length=200, null=True)
+    title_en = models.CharField('Название на английском языке', max_length=200, null=True)
+    title_jp = models.CharField('Название на японском языке', max_length=200, null=True)
+    image = models.ImageField('Картинка')
+    description = models.CharField('Описание', max_length=1000, null=True)
+    previous_evolution = models.ForeignKey('self', verbose_name='Из кого эволюционирует', null=True, blank=True, related_name='next_evolutions', on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return '{}'.format(self.title_ru)
+
+class PokemonEntity(models.Model):
+    '''Сущности покемона'''
+    lat = models.FloatField('Широта')
+    lon = models.FloatField('Долгота')
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    appeared_at = models.DateTimeField('Время появления')
+    disappeared_at = models.DateTimeField('Время исчезновения')
+    level = models.IntegerField('Уровень', null=True)
+    health = models.IntegerField('Здоровье', null=True)
+    strength = models.IntegerField('Атака', null=True)
+    defence = models.IntegerField('Защита', null=True)
+    stamina = models.IntegerField('Выносливость', null=True)
