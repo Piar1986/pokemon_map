381993604f3527a9e631982239f06bf0b8d10f72
IvanPolienko <piar1986@yandex.ru>
2020-03-12 00:22:46 +0700

    Allowed fields to be empty

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Pokemon(models.Model):
     '''Покемон.'''
-    title_ru = models.CharField('Название на русском языке', max_length=200)
-    title_en = models.CharField('Название на английском языке', max_length=200)
-    title_jp = models.CharField('Название на японском языке', max_length=200)
-    image = models.ImageField('Картинка')
-    description = models.CharField('Описание', max_length=1000)
+    title_ru = models.CharField('Название на русском языке', max_length=200, blank=True)
+    title_en = models.CharField('Название на английском языке', max_length=200, blank=True)
+    title_jp = models.CharField('Название на японском языке', max_length=200, blank=True)
+    image = models.ImageField('Картинка', blank=True)
+    description = models.CharField('Описание', max_length=1000, blank=True)
     previous_evolution = models.ForeignKey('self', verbose_name='Из кого эволюционирует', null=True, blank=True, related_name='next_evolutions', on_delete=models.SET_NULL)
 
     def __str__(self):
